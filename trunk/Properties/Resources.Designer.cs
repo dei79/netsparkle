aa4d2f9413f01bd1c843364e04cc893bcf28988b
dei79_cp <SND\dei79_cp>
2010-11-01 18:27:55 +0000

    renamed output file and default namespace

--- trunk/Properties/Resources.Designer.cs
+++ trunk/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NetSparkle.Properties {
+namespace AppLimit.NetSparkle.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace NetSparkle.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NetSparkle.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AppLimit.NetSparkle.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
