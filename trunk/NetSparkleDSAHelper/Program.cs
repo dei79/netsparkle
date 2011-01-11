dff0f80f3405592e474375214d0fd7233f71be57
dei79_cp <SND\dei79_cp>
2011-01-11 00:39:51 +0000

    added verification code

--- trunk/NetSparkleDSAHelper/Program.cs
+++ trunk/NetSparkleDSAHelper/Program.cs
@@ -13,105 +13,139 @@ namespace NetSparkleDSAHelper
         private static String _dsaPubKey = "NetSparkle_DSA.pub";
 
         static void Main(string[] args)
-        {            
-            // check if we have some parameters
-            if (args.Count() < 1)
+        {
+            try
             {
-                Usage();
-                return;
-            }
-
-            // check what parameter we have
-            switch (args[0].ToLower())
-            {
-                case "/genkey_pair":
-                    {
-                        // show headline
-                        ShowHeadLine();
-
-                        // verify if output file exists
-                        if (File.Exists(_dsaPrivKey) || File.Exists(_dsaPubKey))
-                        {
-                            Console.WriteLine("Error: Output files are currently exists");
-                            Environment.ExitCode = -1;
-                            return;
-                        }
-
-                        // start key generation
-                        Console.WriteLine("Generating key pair with 1024 Bits...");
-                        DSACryptoServiceProvider prv = new DSACryptoServiceProvider();
-
-                        Console.WriteLine("Storing private key to " + _dsaPrivKey);
-                        using (StreamWriter sw = new StreamWriter(_dsaPrivKey))
-                        {
-                            sw.Write(prv.ToXmlString(true));
-                        }
-
-                        Console.WriteLine("Storing public key to " + _dsaPubKey);
-                        using (StreamWriter sw = new StreamWriter(_dsaPubKey))
-                        {
-                            sw.Write(prv.ToXmlString(false));
-                        }
-
-                        Console.WriteLine("");
-                    }
-                    break;
-                case "/sign_update":
-                    {
-                        if (args.Count() != 3)
-                        {
-                            Usage();
-                            Environment.ExitCode = -1;
-                            return;
-                        }
-
-                        // get parameter
-                        String binary = args[1];
-                        String privKey = args[2];
-
-                        if (!File.Exists(binary))
-                        {
-                            Console.Error.WriteLine("Target binary " + binary + " does not exists");
-                            Environment.ExitCode = -1;
-                            return;
-                        }
-
-                        if (!File.Exists(privKey))
-                        {
-                            Console.Error.WriteLine("Private key file does not exists");
-                            Environment.ExitCode = -1;
-                            return;
-                        }
-
-                        // Reading private key
-                        String key = null;
-                        using(StreamReader reader = new StreamReader(privKey))
-                        {
-                            key = reader.ReadToEnd();
-                        }
-                        
-                        DSACryptoServiceProvider prv = new DSACryptoServiceProvider();
-                        prv.FromXmlString(key);
-
-                        // open stream
-                        Byte[] hash = null;
-                        using (Stream inputStream = File.OpenRead(binary))
-                        {
-                            hash = prv.SignData(inputStream);
-                        }
-
-                        String base64Hash = Convert.ToBase64String(hash);
-                        Console.WriteLine(base64Hash);
-                    }
-                    break;
-                default:
+                // check if we have some parameters
+                if (args.Count() < 1)
+                {
                     Usage();
-                    break;
-            }            
+                    return;
+                }
+
+                // check what parameter we have
+                switch (args[0].ToLower())
+                {
+                    case "/genkey_pair":
+                        {
+                            // show headline
+                            ShowHeadLine();
+
+                            // verify if output file exists
+                            if (File.Exists(_dsaPrivKey) || File.Exists(_dsaPubKey))
+                            {
+                                Console.WriteLine("Error: Output files are currently exists");
+                                Environment.ExitCode = -1;
+                                return;
+                            }
+
+                            // start key generation
+                            Console.WriteLine("Generating key pair with 1024 Bits...");
+                            DSACryptoServiceProvider prv = new DSACryptoServiceProvider();
+
+                            Console.WriteLine("Storing private key to " + _dsaPrivKey);
+                            using (StreamWriter sw = new StreamWriter(_dsaPrivKey))
+                            {
+                                sw.Write(prv.ToXmlString(true));
+                            }
+
+                            Console.WriteLine("Storing public key to " + _dsaPubKey);
+                            using (StreamWriter sw = new StreamWriter(_dsaPubKey))
+                            {
+                                sw.Write(prv.ToXmlString(false));
+                            }
+
+                            Console.WriteLine("");
+                        }
+                        break;
+                    case "/sign_update":
+                        {
+                            if (args.Count() != 3)
+                            {
+                                Usage();
+                                Environment.ExitCode = -1;
+                                return;
+                            }
+
+                            // get parameter
+                            String binary = args[1];
+                            String privKey = args[2];
+
+                            if (!File.Exists(binary))
+                            {
+                                Console.Error.WriteLine("Target binary " + binary + " does not exists");
+                                Environment.ExitCode = -1;
+                                return;
+                            }
+
+                            if (!File.Exists(privKey))
+                            {
+                                Console.Error.WriteLine("Private key file does not exists");
+                                Environment.ExitCode = -1;
+                                return;
+                            }
+
+                            // Reading private key
+                            String key = null;
+                            using (StreamReader reader = new StreamReader(privKey))
+                            {
+                                key = reader.ReadToEnd();
+                            }
+
+                            DSACryptoServiceProvider prv = new DSACryptoServiceProvider();
+                            prv.FromXmlString(key);
+
+                            // open stream
+                            Byte[] hash = null;
+                            using (Stream inputStream = File.OpenRead(binary))
+                            {
+                                hash = prv.SignData(inputStream);
+                            }
+
+                            String base64Hash = Convert.ToBase64String(hash);
+                            Console.WriteLine(base64Hash);
+                        }
+                        break;
+                    case "/verify_update":
+                        {
+                            if (args.Count() != 4)
+                            {
+                                Usage();
+                                Environment.ExitCode = -1;
+                                return;
+                            }
+
+                            // get parameter
+                            String binary = args[1];
+                            String pubKey = args[2];
+                            String sign = args[3];
+
+                            sign = sign.TrimStart('"');
+                            sign = sign.TrimEnd('"');
+
+                            AppLimit.NetSparkle.NetSparkleDSAVerificator dsaVerif = new AppLimit.NetSparkle.NetSparkleDSAVerificator(pubKey);
+                            if (dsaVerif.VerifyDSASignature(sign, binary))
+                                Console.WriteLine("Binary " + binary + " is valid");
+                            else
+                                Console.WriteLine("Binary " + binary + " is NOT valid");
+                        }
+                        break;
+                    default: 
+                        Usage();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong :-(");
+                Console.WriteLine(e.StackTrace);
+            }
         }
 
         static private void Usage()
-        {            
+        {
+            ShowHeadLine();
+
             Console.WriteLine("NetSparkleDSAHelper.exe /genkey_pair");
             Console.WriteLine("");
             Console.WriteLine("Generates a public and a private DSA key pair which is stored in the current");
@@ -125,7 +159,10 @@ namespace NetSparkleDSAHelper
             Console.WriteLine("a valid path to the package binary as self (mostly Windows Installer packages).");
             Console.WriteLine("The NetSparkle_DSA.priv has to be a path to the generated DAS private key,");
             Console.WriteLine("which has to be used for signing.");
-            Console.WriteLine("");            
+            Console.WriteLine("");
+            Console.WriteLine("NetSparkleDSAHelper.exe /verify_update {YourPackage.msi} {NetSparkle_DSA.pub} \"{Base64SignatureString}\"");
+            Console.WriteLine("");
+            
         }
 
         private static void ShowHeadLine()
