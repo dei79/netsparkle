48b936c45930734e47e4881f880c4ed4b48fa579
Renald_cp <SND\Renald_cp>
2011-07-25 11:28:09 +0000

    Get "osversion" and "ncpu" for system profiling

--- trunk/NetSparkle/NetSparkleDeviceInventory.cs
+++ trunk/NetSparkle/NetSparkleDeviceInventory.cs
@@ -13,6 +13,8 @@ namespace AppLimit.NetSparkle
         public Boolean x64System { get; set; }
         public uint ProcessorSpeed { get; set; }
         public Int64 MemorySize { get; set; }
+        public string OsVersion { get; set; }
+        public int CPUCount { get; set; }
 
         private NetSparkleConfiguration _config;
 
@@ -29,8 +31,14 @@ namespace AppLimit.NetSparkle
             // cpu speed
             CollectCPUSpeed();
 
+            // cpu count
+            CollectCPUCount();
+
             // ram size
             CollectRamSize();
+
+            // windows
+            CollectWindowsVersion();
         }
 
         public String BuildRequestUrl(String baseRequestUrl)
@@ -56,16 +64,17 @@ namespace AppLimit.NetSparkle
             retValue += "lang=" + Thread.CurrentThread.CurrentUICulture.ToString() + "&";
 
             // Windows version
-            // ### TODO ###
+            retValue += "osVersion=" + OsVersion + "&";
 
             // CPU type/subtype (see mach/machine.h for decoder information on this data)
-            // ### TODO ###
+            // ### TODO: cputype, cpusubtype ###
 
             // Mac model
-            // ### TODO ###
+            // ### TODO: model ###
 
             // Number of CPUs (or CPU cores, in the case of something like a Core Duo)
-            // ### TODO ###
+            // ### TODO: ncpu ###
+            retValue += "ncpu=" + CPUCount + "&";
 
             // sanitize url
             retValue = retValue.TrimEnd('&');            
@@ -74,6 +83,11 @@ namespace AppLimit.NetSparkle
             return retValue;
         }
 
+        private void CollectWindowsVersion()
+        {
+            OperatingSystem osInfo = Environment.OSVersion;
+            OsVersion = string.Format("{0}.{1}.{2}.{3}", osInfo.Version.Major, osInfo.Version.Minor, osInfo.Version.Build, osInfo.Version.Revision);
+        }
 
         private void CollectProcessorBitnes()
         {
@@ -81,6 +95,11 @@ namespace AppLimit.NetSparkle
                 x64System = true;
             else
                 x64System = false;
+        }
+
+        private void CollectCPUCount()
+        {
+            CPUCount = Environment.ProcessorCount;
         }
 
         private void CollectCPUSpeed()
